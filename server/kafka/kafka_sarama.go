a8409d537ff814942c2893209de581a16d00f999
ZMing316 <zming316@hotmail.com>
2022-05-17 09:34:51 +0000

    add mechanism support SHA256, 512

--- server/kafka/kafka_sarama.go
+++ server/kafka/kafka_sarama.go
@@ -1,13 +1,48 @@
 package kafka
 
 import (
+	"crypto/sha256"
+	"crypto/sha512"
 	"crypto/tls"
 	"fmt"
+	"hash"
+	"strings"
 	"time"
 
 	"github.com/Shopify/sarama"
 	"github.com/nats-io/nats-kafka/server/conf"
+	"github.com/xdg-go/scram"
 )
+
+// Predefined SCRAMClientGeneratorFunc, copied from https://github.com/Shopify/sarama/blob/master/examples/sasl_scram_client/scram_client.go
+
+var SHA256 scram.HashGeneratorFcn = func() hash.Hash { return sha256.New() }
+var SHA512 scram.HashGeneratorFcn = func() hash.Hash { return sha512.New() }
+
+type XDGSCRAMClient struct {
+	*scram.Client
+	*scram.ClientConversation
+	scram.HashGeneratorFcn
+}
+
+func (x *XDGSCRAMClient) Begin(userName, password, authzID string) (err error) {
+	x.Client, err = x.HashGeneratorFcn.NewClient(userName, password, authzID)
+	if err != nil {
+		return err
+	}
+	x.ClientConversation = x.Client.NewConversation()
+	return nil
+}
+
+func (x *XDGSCRAMClient) Step(challenge string) (response string, err error) {
+	response, err = x.ClientConversation.Step(challenge)
+	return
+}
+
+func (x *XDGSCRAMClient) Done() bool {
+	return x.ClientConversation.Done()
+}
+
 
 // GetSaramaConfig creates a common sarama.Config
 func GetSaramaConfig(cc conf.ConnectorConfig, clientID string, dialTimeout time.Duration) (*sarama.Config, error) {
@@ -16,10 +51,15 @@ func GetSaramaConfig(cc conf.ConnectorConfig, clientID string, dialTimeout time.
 	sc.Net.DialTimeout = dialTimeout
 
 	if cc.SASL.Mechanism != "" {
-		switch cc.SASL.Mechanism {
+		switch strings.ToUpper(cc.SASL.Mechanism) {
 		case "PLAIN":
+			sc.Net.SASL.Mechanism = sarama.SASLTypePlaintext
 		case "SCRAM-SHA-256":
+			sc.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA256
+			sc.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA256} }
 		case "SCRAM-SHA-512":
+			sc.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA512
+			sc.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA256} }
 		default:
 			return nil, fmt.Errorf("invalid SASL.Mechanism %s", cc.SASL.Mechanism)
 		}
@@ -29,7 +69,6 @@ func GetSaramaConfig(cc conf.ConnectorConfig, clientID string, dialTimeout time.
 			sc.Net.SASL.Version = sarama.SASLHandshakeV1
 		}
 
-		sc.Net.SASL.Mechanism = (sarama.SASLMechanism)(cc.SASL.Mechanism)
 		sc.Net.SASL.User = cc.SASL.User
 		sc.Net.SASL.Password = cc.SASL.Password
 		sc.Net.SASL.GSSAPI = cc.SASL.GSSAPI
